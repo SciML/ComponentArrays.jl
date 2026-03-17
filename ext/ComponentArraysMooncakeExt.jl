66acfacf545f2643ee59a316bb8a364897df44f2
Astitva Aggarwal <astitvaaggarwalimportant@gmail.com>
2026-03-17 20:14:17 +0000

    fin

--- ext/ComponentArraysMooncakeExt.jl
+++ ext/ComponentArraysMooncakeExt.jl
@@ -3,13 +3,12 @@ module ComponentArraysMooncakeExt
 using ComponentArrays, Mooncake
 
 # ComponentVector handling in @from_rrule
-function increment_and_get_rdata!(
-    f::Mooncake.FData{@NamedTuple{data::Vector{T},axes::Mooncake.NoFData}},
+function Mooncake.increment_and_get_rdata!(
+    f::Mooncake.FData{@NamedTuple{data::A, axes::Mooncake.NoFData}},
     r::Mooncake.NoRData,
-    t::Vector{T},
-) where {T<:Base.IEEEFloat}
-    f.data[:data] .+= t
-    return r
+    t::A,
+) where {P<:Union{Base.IEEEFloat,Complex{<:Base.IEEEFloat}},A<:Array{P}}
+    return Mooncake.increment_and_get_rdata!(f.data[:data], r, t)
 end
 
 end
